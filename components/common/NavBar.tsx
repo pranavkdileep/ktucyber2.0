3a440247b6f2c5fd85e3e559687bd15e9b32bb62
pranavkdileep <pranavdileep10@gmail.com>
2025-05-30 16:46:56 +0530

    vercel

--- components/common/NavBar.tsx
+++ components/common/NavBar.tsx
@@ -18,7 +18,6 @@ function NavBar() {
   useEffect(() => {
     const checkAuthStatus = async () => {
       const response = await verifyToken();
-      console.log("Auth check response:", response);
       if (response.success) {
         setIsAuthenticated(true);
         setUserData(response.payload);
